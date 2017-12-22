040e41ec581c190869947aba70bd96ca2282e464
alok.bichhwe <alok.bichhwe@systematixindia.com>
2017-12-22 19:49:03 +0530

    update reading response

--- api/controllers/V1/webservices/ReadingController.js
+++ api/controllers/V1/webservices/ReadingController.js
@@ -18,14 +18,34 @@ module.exports = {
   tankReading: function (req, res) {
     var deviceId = req.param('deviceId');
     var timeFilter = req.param('timeFilter');
-    var data = [
-      [{ "v": 49, "name": "1 PM" },
-        { "v": 42, "name": "2 PM" },
-        { "v": 29, "name": "3 PM" },
-        { "v": 50, "name": "4 PM" },
-        { "v": 60, "name": "5 PM" }
+    if('day'){
+      var data = [
+        [{ "v": 49, "name": "1 PM" },
+          { "v": 42, "name": "2 PM" },
+          { "v": 29, "name": "3 PM" },
+          { "v": 50, "name": "4 PM" },
+          { "v": 60, "name": "5 PM" }
         ]
-    ];
+      ];
+    }else if('week'){
+      var data = [
+        [{ "v": 49, "name": "1 Week" },
+          { "v": 42, "name": "2 Week" },
+          { "v": 29, "name": "3 Week" },
+          { "v": 50, "name": "4 Week" },
+          { "v": 60, "name": "5 Week" }
+        ]
+      ];
+    }else if('month'){
+      var data = [
+        [{ "v": 49, "name": "Aug" },
+          { "v": 42, "name": "Sep" },
+          { "v": 29, "name": "Oct" },
+          { "v": 50, "name": "Nov" },
+          { "v": 60, "name": "Dec" }
+        ]
+      ];
+    }
     return res.json(data);
   }
 
