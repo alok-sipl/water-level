f5b25cf52741c0af89763cf6bc00bc08645abf95
alok.bichhwe <alok.bichhwe@systematixindia.com>
2017-12-28 19:30:20 +0530

    update valiation errro

--- api/controllers/V1/webservices/ReadingController.js
+++ api/controllers/V1/webservices/ReadingController.js
@@ -5,7 +5,7 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 var db = sails.config.globals.firebasedb();
-
+// Create a GeoFire index
 module.exports = {
 
   /*
@@ -15,10 +15,130 @@ module.exports = {
      * Purpose: add new supplier
      * @param  req
      */
+  deviceReading: function(req, res) {
+    var ref = db.ref();
+    var likes = ref.child("device_reading");
+    var newSubCities = likes.push();
+    newSubCities.set({
+        device_id: "-L0oJuC3wNmUAnuic-or",
+        reading: "55",
+        created_date: Date.now()
+    });
+    var newSubCities = likes.push();
+    newSubCities.set({
+        device_id: "-L0oJuC3wNmUAnuic-or",
+        reading: "16",
+        created_date: Date.now()
+    });
+    var newSubCities = likes.push();
+    newSubCities.set({
+        device_id: "-L0oJuC3wNmUAnuic-or",
+        reading: "3",
+        created_date: Date.now()
+    });
+    var newSubCities = likes.push();
+    newSubCities.set({
+        device_id: "-L0oJuC3wNmUAnuic-or",
+        reading: "88",
+        created_date: Date.now()
+    });
+    newSubCities.set({
+        device_id: "-L0oJuC3wNmUAnuic-or",
+        reading: "12",
+        created_date: Date.now()
+    });
+    var newSubCities = likes.push();
+    newSubCities.set({
+        device_id: "-L0oJuC3wNmUAnuic-or",
+        reading: "50",
+        created_date: Date.now()
+    });
+    var newSubCities = likes.push();
+    newSubCities.set({
+        device_id: "-L0oJuC3wNmUAnuic-or",
+        reading: "56",
+        created_date: Date.now()
+    });
+    var newSubCities = likes.push();
+    newSubCities.set({
+        device_id: "-L0oJuC3wNmUAnuic-or",
+        reading: "98",
+        created_date: Date.now()
+    });
+    newSubCities.set({
+        device_id: "-L0oJuC3wNmUAnuic-or",
+        reading: "99",
+        created_date: Date.now()
+    });
+    var newSubCities = likes.push();
+    newSubCities.set({
+        device_id: "-L0oJuC3wNmUAnuic-or",
+        reading: "74",
+        created_date: Date.now()
+    });
+    var newSubCities = likes.push();
+    newSubCities.set({
+        device_id: "-L0oJuC3wNmUAnuic-or",
+        reading: "71",
+        created_date: Date.now()
+    });
+    var newSubCities = likes.push();
+    newSubCities.set({
+        device_id: "-L0oJuC3wNmUAnuic-or",
+        reading: "89",
+        created_date: Date.now()
+    });
+},
   tankReading: function (req, res) {
     var deviceId = req.param('deviceId');
     var timeFilter = req.param('timeFilter');
     var data = [];
+    var jsonData = [];
+    var deviceReadingList = [];
+    var array = [];
+    var deviceReadings;
+    // Get current date time in ISO format
+    var currentDateTimeStamp = new Date();
+    var currentDateTime = currentDateTimeStamp.toISOString();
+    //convert ISO format  to timestamp
+    var currentDateTimeString = Date.parse(currentDateTime);
+    // Current timestamp
+    var currentTimeStamp  =   currentDateTimeString/1000;
+
+    var pastSixHrsDateTime = new Date(new Date() - (10 * 60 * 60 * 1000)).toISOString();
+    var pastDateTimeString = Date.parse(pastSixHrsDateTime);
+    // past 6 hrs timestamp
+    var pastTimeStamp  =   pastDateTimeString/1000;
+
+    var refDeviceReading = db.ref("device_reading");
+
+    // refDeviceReading
+    //   .orderByChild('device_id')
+    //   .startAt('-L0oJuC3wNmUAnuic-or').endAt('-L0oJuC3wNmUAnuic-or')
+    //   .orderByChild('created_date')                  // !!! THIS LINE WILL RAISE AN ERROR !!!
+    //   .startAt(pastTimeStamp).endAt(currentTimeStamp)
+    //   .on('value', function(snapshot) {
+    //       deviceReadings = snapshot.val();
+    //       deviceReadingList.push(deviceReadings)
+    //   });
+
+    refDeviceReading.orderByChild('device_id').equalTo('-L0oJuC3wNmUAnuic-or').on("value", function(snapshot) {
+      deviceReadings = snapshot.val();
+      Object.keys(deviceReadings).forEach(key => {
+        var readingDataObject =  {};
+
+        if(deviceReadings[key].created_date >= pastDateTimeString && deviceReadings[key].created_date <= currentDateTimeString ){
+          //readingDataObject["reading_id"] = key;
+          readingDataObject["name"] = new Date(deviceReadings[key].created_date).getUTCHours();
+        //  readingDataObject["device_id"]  = deviceReadings[key].device_id;;
+          readingDataObject["v"] = deviceReadings[key].reading;
+          deviceReadingList.push(readingDataObject)
+        }
+
+      });
+    });
+
+
     if('day'){
       data = [
         [{ "v": 49, "name": "1 PM" },
@@ -47,70 +167,79 @@ module.exports = {
         ]
       ];
     }
-    return res.json(data);
-  },
+    console.log("CURRENT "+currentDateTime +" Cureent Timestamp "+ currentTimeStamp);
+    console.log("PAST "+pastSixHrsDateTime +" PAST Timestamp "+ pastTimeStamp);
 
+    return res.json(deviceReadingList);
+  },
   supplierListing: function (req, res) {
     var currLat1 = "22.743564";
     var currLong1 = "75.89674600000001";
     var jsonData = [];
     var favouritesSuppliresId = [];
     var array = [];
-    var favouritesSupplires = "";
+    var favouritesSupplires ="";
     var reflikedSuppliers = db.ref("likes");
-    reflikedSuppliers.orderByChild('user_id').equalTo('-L0UbCoAiFk06mBEYfDZ').on("value", function (snapshot) {
+
+    reflikedSuppliers.orderByChild('user_id').equalTo('-L0UbCoAiFk06mBEYfDZ').on("value", function(snapshot) {
       favouritesSupplires = snapshot.val();
       console.log(favouritesSupplires);
-      Object.keys(favouritesSupplires).forEach(key = > {if(favouritesSuppliresId.indexOf(favouritesSupplires[key].supplier_id) === -1
-    )
-      {
-        favouritesSuppliresId.push(favouritesSupplires[key].supplier_id);
-      }
-    })
-      ;
+
+      Object.keys(favouritesSupplires).forEach(key => {
+        if(favouritesSuppliresId.indexOf(favouritesSupplires[key].supplier_id) === -1){
+            favouritesSuppliresId.push(favouritesSupplires[key].supplier_id);
+         }
+      });
     });
+
     var refSuppliers = db.ref("suppliers");
     refSuppliers.orderByKey().on("value", function (snapshot) {
-      var suppliers = snapshot.val();
-      Object.keys(suppliers).forEach(key = > {var supplierWithDistance = {};
-      distance = ValidationService.getDistanceFromLatLonInKm(currLat1, currLong1, suppliers[key].latitude, suppliers[key].longitude);
-      supplierWithDistance["supplier_id"] = key;
-      supplierWithDistance["distance_in_km"] = distance;
-      supplierWithDistance["account_number"] = suppliers[key].account_number;
-      supplierWithDistance["area"] = suppliers[key].area;
-      supplierWithDistance["city_id"] = suppliers[key].city_id;
-      supplierWithDistance["city_name"] = suppliers[key].city_name;
-      supplierWithDistance["company_name"] = suppliers[key].company_name;
-      supplierWithDistance["country_id"] = suppliers[key].country_id;
-      supplierWithDistance["country_name"] = suppliers[key].country_name;
-      supplierWithDistance["created_date"] = suppliers[key].created_date;
-      supplierWithDistance["email"] = suppliers[key].email;
-      supplierWithDistance["latitude"] = suppliers[key].latitude;
-      supplierWithDistance["longitude"] = suppliers[key].longitude;
-      supplierWithDistance["mobile_number"] = suppliers[key].mobile_number;
-      supplierWithDistance["name"] = suppliers[key].name;
-      if (favouritesSuppliresId.indexOf(key)) {
-        supplierWithDistance["is_fav"] = 1;
-      } else {
-        supplierWithDistance["is_fav"] = 0;
-      }
-      if (distance && distance < 40) {
-        jsonData.push(supplierWithDistance);
-      }
-    })
-      ;array.push(jsonData);
-      array[0].sort(function (x, y) {
+    var suppliers = snapshot.val();
+
+     Object.keys(suppliers).forEach(key => {
+        var supplierWithDistance = {};
+        distance =   ValidationService.getDistanceFromLatLonInKm(currLat1,currLong1,suppliers[key].latitude,suppliers[key].longitude);
+
+        supplierWithDistance["supplier_id"] = key;
+        supplierWithDistance["distance_in_km"] = distance;
+        supplierWithDistance["account_number"] = suppliers[key].account_number;
+        supplierWithDistance["area"] = suppliers[key].area;
+        supplierWithDistance["city_id"] = suppliers[key].city_id;
+        supplierWithDistance["city_name"] = suppliers[key].city_name;
+        supplierWithDistance["company_name"] = suppliers[key].company_name;
+        supplierWithDistance["country_id"] = suppliers[key].country_id;
+        supplierWithDistance["country_name"] = suppliers[key].country_name;
+        supplierWithDistance["created_date"] = suppliers[key].created_date;
+        supplierWithDistance["email"] = suppliers[key].email;
+        supplierWithDistance["latitude"] = suppliers[key].latitude;
+        supplierWithDistance["longitude"] = suppliers[key].longitude;
+        supplierWithDistance["mobile_number"] = suppliers[key].mobile_number;
+        supplierWithDistance["name"] = suppliers[key].name;
+        if(favouritesSuppliresId.indexOf(key)){
+          supplierWithDistance["is_fav"] = 1;
+        }else{
+          supplierWithDistance["is_fav"] = 0;
+        }
+        if(distance &&  distance < 40 ){
+          jsonData.push(supplierWithDistance);
+        }
+  });
+       array.push(jsonData);
+       array[0].sort(function (x, y) {
         var n = x.distance_in_km - y.distance_in_km;
         if (n != 0) {
-          return n;
+            return n;
         }
+
         return x.is_fav - y.is_fav;
-      });
+    });
+
       return res.json(array[0]);
-    }, function (errorObject) {
+   }, function (errorObject) {
       return res.serverError(errorObject.code);
     });
-  },
+
+},
+
 
 };
-
--- api/services/ValidationService.js
+++ api/services/ValidationService.js
@@ -119,14 +119,13 @@ module.exports = {
     }
     return errors;
   },
-
   getDistanceFromLatLonInKm : function (lat1,lon1,lat2,lon2) {
     var R = 6371; // Radius of the earth in km
     var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
     var dLon = this.deg2rad(lon2-lon1);
     var a =
       Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(this.deg2rad(lat1))  Math.cos(this.deg2rad(lat2)) 
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
       Math.sin(dLon/2) * Math.sin(dLon/2)
       ;
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
@@ -136,4 +135,5 @@ module.exports = {
   deg2rad : function (deg) {
     return deg * (Math.PI/180)
   },
+
 }
